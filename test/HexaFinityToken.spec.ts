10881e41b737237b13df986c6e63ad3ccc83fd23
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-04-30 05:11:45 +0900

    transferFrom test-case

--- test/HexaFinityToken.spec.ts
+++ test/HexaFinityToken.spec.ts
@@ -82,4 +82,22 @@ describe('HexaFinityToken', () => {
     await expect(token.connect(other).transfer(wallet.address, 1)).to.be
       .reverted;
   });
+
+  it('transferFrom', async () => {
+    await token.approve(other.address, TEST_AMOUNT);
+    await expect(
+      token
+        .connect(other)
+        .transferFrom(wallet.address, other.address, TEST_AMOUNT),
+    )
+      .to.emit(token, 'Transfer')
+      .withArgs(wallet.address, other.address, TEST_AMOUNT);
+    expect(await token.allowance(wallet.address, other.address)).to.eq(
+      0,
+    );
+    expect(await token.balanceOf(wallet.address)).to.eq(
+      TOTAL_SUPPLY.sub(TEST_AMOUNT),
+    );
+    expect(await token.balanceOf(other.address)).to.eq(TEST_AMOUNT);
+  });
 });
