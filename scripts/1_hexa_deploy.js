79b8d564151b1286cdd4a1ee054d63a41b2a5236
Kevin Qiang <butterflybutterpot@gmail.com>
2022-05-28 06:39:19 +0900

    update tax receiver address

--- scripts/1_hexa_deploy.js
+++ scripts/1_hexa_deploy.js
@@ -10,7 +10,7 @@ async function main() {
   */
   const routerAddress = '0x89F70d27c3661595eA37C131edD55A0B3AFeBC34'; // pancakeswap v2 router for testnet
   // const routerAddress = "0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F"; // panckaeswap v2 router for mainnet
-  const taxReceiverAddress = '0xe5D1cb60cb065bf23d3022D02a205D829Feb9831'; // temp tax receiver address
+  const taxReceiverAddress = '0x35a8276acc795618bcfeac47be808d5a7e77ff0a'; // temp tax receiver address
   // const taxReceiverAddress = "0xdbb5633eee15f0649d8747e6f65abdf0078c264a"; // tax receiver address
 
   // Start Deploying
