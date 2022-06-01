83c279c0701ff004fb03be5f22b2721a4105b153
Kevin Qiang <butterflybutterpot@gmail.com>
2022-06-02 06:10:09 +0900

    update with openzeppelin upgradeable

--- scripts/1_hexa_deploy.js
+++ scripts/1_hexa_deploy.js
@@ -10,8 +10,8 @@ async function main() {
   */
   const routerAddress = '0x89F70d27c3661595eA37C131edD55A0B3AFeBC34'; // pancakeswap v2 router for testnet
   // const routerAddress = "0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F"; // panckaeswap v2 router for mainnet
-  const taxReceiverAddress = '0x35a8276acc795618bcfeac47be808d5a7e77ff0a'; // temp tax receiver address
-  // const taxReceiverAddress = "0xdbb5633eee15f0649d8747e6f65abdf0078c264a"; // tax receiver address
+  const taxReceiverAddress = '0x2155BCeA4f362D5D9CE67817b826A8F31b61D0BF'; // temp tax receiver address
+  // const taxReceiverAddress = "0x35a8276acc795618bcfeac47be808d5a7e77ff0a"; // tax receiver address
 
   // Start Deploying
   console.log('Deploying contracts with the account...');
